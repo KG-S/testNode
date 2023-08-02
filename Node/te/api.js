76bd4a18c332c96c0cbcf51b67f8b5ba63c78a7a
KG-S <k.gabriel@aluno.ifsp.edu.br>
2023-08-02 16:15:20 -0300

    Versão 03

--- Node/te/api.js
+++ Node/te/api.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var url = 1;
     var img='';
     var titlr= '';
-    var author='';
+    var author='1212S';
 
     $.get("https://www.googleapis.com/books/v1", function(response){})
     console.log(response)
